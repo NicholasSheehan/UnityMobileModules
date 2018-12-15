3527bb5532d0f2cea8b1ba2e89f518e620ea5dfd
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-12-15 21:55:44 +0000

    *Added Demo methods for App Validity

---
+++ Assets/UnityMobileModuleDemo/App Validity/DisplayAppValidityState.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityMobileModules.Demo
+{
+    [RequireComponent(typeof(Text))]
+    public class DisplayAppValidityState : MonoBehaviour
+    {
+        /// <summary>
+        /// State Text
+        /// </summary>
+        Text stateText;
+
+        void Awake()
+        {
+            stateText = GetComponent<Text>();
+        }
+
+        void Start()
+        {
+            UpdateText();
+        }
+
+        /// <summary>
+        /// Updates the AppValidity state
+        /// </summary>
+        void UpdateText()
+        {
+            stateText.text = AppValidity.validityState.ToString();
+        }
+    }
+}
