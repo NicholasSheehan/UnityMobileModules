04edbd4e05467a1c0f4d73f4bbf2b63f858e4362
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2018-12-21 16:04:39 +0000

    *added Keyboard functionality

---
+++ Assets/UnityMobileModules/Keyboard/Keyboard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityMobileModules
+{
+    public static class Keyboard
+    {
+        /// <summary>
+        /// Is the keyboard supported
+        /// </summary>
+        public static bool isSupported
+        {
+            get
+            {
+                return TouchScreenKeyboard.isSupported;
+            }
+        }
+
+        /// <summary>
+        /// Is the keyboard visible
+        /// </summary>
+        public static bool isVisible
+        {
+            get
+            {
+                return TouchScreenKeyboard.visible;
+            }
+        }
+
+        /// <summary>
+        /// Opens a keyboard
+        /// </summary>
+        /// <param name="text">	Text to edit.</param>
+        /// <param name="keyboardType">Type of keyboard (eg, any text, numbers only, etc).</param>
+        /// <param name="autocorrection">Is autocorrection applied?</param>
+        /// <param name="multiline">Can more than one line of text be entered?</param>
+        /// <param name="secure">Is the text masked (for passwords, etc)?</param>
+        /// <param name="alert">Is the keyboard opened in alert mode?</param>
+        /// <param name="textPlaceholder">Text to be used if no other text is present.</param>
+        /// <returns></returns>
+        public static TouchScreenKeyboard Open(string text, TouchScreenKeyboardType keyboardType = TouchScreenKeyboardType.Default, bool autocorrection = true, bool multiline = false, bool secure = false, bool alert = false, string textPlaceholder = "\"\"")
+        {
+            return TouchScreenKeyboard.Open(text, keyboardType, autocorrection, multiline, secure, alert);
+        }
+    }
+}
