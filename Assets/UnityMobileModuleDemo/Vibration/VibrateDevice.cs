7708b326a9c1ad83857a9ab9588ec34c013d773e
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2019-03-03 23:25:47 +0000

    *added Vibration demo panel *ignored Resharper generated files

---
+++ Assets/UnityMobileModuleDemo/Vibration/VibrateDevice.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityMobileModules.Demo
+{
+    [RequireComponent(typeof(Button))]
+    public class VibrateDevice : MonoBehaviour
+    {
+        /// <summary>
+        /// Vibration Type
+        /// </summary>
+        [SerializeField] VibrationType vibrationType;
+
+        /// <summary>
+        /// Vibrates the device, called via button
+        /// </summary>
+        public void Vibrate()
+        {
+            Vibration.Vibrate(vibrationType);
+        }
+    }
+}
