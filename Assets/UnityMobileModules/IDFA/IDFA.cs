10fd083953d332dfe71df1fc3b4f9d975337fe0b
Nicholas Sheehan <NicholasSheehan@live.co.uk>
2019-03-23 21:21:23 +0000

    *Start of IDFA

---
+++ Assets/UnityMobileModules/IDFA/IDFA.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityMobileModules
+{
+    /// <summary>
+    /// Gets the Identifier For Advertisers (IDFA)
+    /// </summary>
+    public static partial class IDFA
+    {
+        /// <summary>
+        /// Returns the IDFA of the device
+        /// </summary>
+        /// <returns></returns>
+        public static string advertisingIdentifier
+        {
+            get
+            {
+#if UNITY_ANDROID
+                return advertisingIdentifier_Android;
+#elif UNITY_IOS
+                return UnityEngine.iOS.Device.advertisingIdentifier;
+#endif
+            }
+        }
+    }
+}
---
+++ Assets/UnityMobileModules/IDFA/IDFA_AndroidBridge.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+namespace UnityMobileModules
+{
+#if UNITY_ANDROID
+    public static partial class IDFA
+    {
+        /// <summary>
+        /// Package Name
+        /// </summary>
+        const string javaPackageName = "com.UnityMobileModules";
+
+        /// <summary>
+        /// Class Name
+        /// </summary>
+        const string javaClassName = "IDFA";
+
+        /// <summary>
+        /// Method Name to find all installed app display names
+        /// </summary>
+        const string GetIDFA_MethodName = "GetIDFA";
+
+        /// <summary>
+        /// Java Class to interface with
+        /// </summary>
+        static readonly AndroidJavaClass javaClass;
+
+        /// <summary>
+        /// Static constructor, guaranteed to run when one of the properties are called
+        /// </summary>
+        static IDFA()
+        {
+#if !UNITY_EDITOR
+            javaClass = new AndroidJavaClass(javaPackageName + "." + javaClassName);
+#endif
+        }
+
+        /// <summary>
+        /// Gets the IDFA for Android
+        /// </summary>
+        static string advertisingIdentifier_Android
+        {
+            get
+            {
+                return javaClass.CallStatic<string>(GetIDFA_MethodName);
+            }
+        }
+    }
+#endif
+}
